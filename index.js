28b54d949642177737cf4dae27b52719f1e3b233
Olivier LABROSSE <labrosseolivier2004@gmail.com>
2023-11-13 16:55:16 +0000

    CA MARCH STP

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const { resolve } = require('path');
-const prisma = require('prisma');
+const { PrismaClient } = require('@prisma/client')
 
-const prism = prisma()
 const app = express();
 var birds = require('./birds');
+const prisma = new PrismaClient()
 
 const port = 3010;
 
 app.use(express.static('static'));
 app.use('/birds', birds);
 app.use([logmiddlewar]);
+
+async function addContact(namee,emaill,phonee)
+{
+  await prisma.contact.create({
+    data: {
+      name: namee,
+      email: emaill,
+      phone: phonee
+    },
+  })
+}
+
+async function addContact(namee,emaill,phonee)
+{
+  await prisma.contact.create({
+    data: {
+      name: namee,
+      email: emaill,
+      phone: phonee
+    },
+  })
+}
 
 function logmiddlewar(req, res, next) {
   console.log(req.socket.localAddress);
@@ -24,10 +46,10 @@ function logmiddlewar(req, res, next) {
 }
 
 function testnumberinfeg10(req, res, next) {
-  if (req.params.id <= 10 && isNaN(req.params.id) == false) { 
+  if (req.params.id <= 10) { 
     next();
   } else {
-    res.sendFile(resolve(__dirname, 'pages/index.html'));
+    res.sendFile(resolve(__dirname, 'pages/ShowContact.html'));
   }
 }
 
@@ -35,17 +57,37 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.get('/test', (req, res, next) => {
-  next();
-}),
-  app.get('/test', (req, res) => {
-    res.sendFile(resolve(__dirname, 'pages/index.html'));
-  });
+// app.get('/test', (req, res, next) => {
+//   next();
+// }),
+//   app.get('/test', (req, res) => {
+//     res.sendFile(resolve(__dirname, 'pages/index.html'));
+//   });
+
+app.get('/test', (req, res) => {
+     res.sendFile(resolve(__dirname, 'pages/test.html'));
+});
+
 
 app.get('/test/:id', [testnumberinfeg10], (req, res) => {
   res.send('Id : ' + req.params.id);
 });
 
+app.get('/gerer', (req, res) => {
+  res.sendFile(resolve(__dirname,ShowContact.html));
+});
+
+//https://3010-olwinss-devweb-fm0371rff45.ws-eu106.gitpod.io/gerer/Olivier/labrosseolivier2004@gmail.com/0768648999
+app.get('/gerer/:name/:email/:phone', (req, res) => {
+  addContact(req.params.name,req.params.email,req.params.phone);
+  res.sendFile(resolve(__dirname, 'pages/test.html'));
+
+});
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  
 });
+
